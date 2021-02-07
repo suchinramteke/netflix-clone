4156ef3e95c2e7efa0b2d2f2b8341fe3b69b61bb
suchinramteke <sachinramteke2908@gmail.com>
2021-02-07 13:33:37 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7wESkpmoUVwLjXSOo2ZQY4DrWm8OELx0",
+    authDomain: "netflix-clone-31fc0.firebaseapp.com",
+    projectId: "netflix-clone-31fc0",
+    storageBucket: "netflix-clone-31fc0.appspot.com",
+    messagingSenderId: "845879409265",
+    appId: "1:845879409265:web:32b1bbcc7e654d6558f9d0"
+};
---
+++ src/Nav.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true)
+        }
+        else {
+            handleShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+
+        return () => window.removeEventListener('scroll', transitionNavBar) 
+
+
+    }, [])
+
+    return (
+        <div className={`nav ${show &&  'nav__black'}`}>
+            <div className="nav__content">
+
+                <img className="nav__logo"
+                    src="https://www.citypng.com/public/uploads/small/11594682142zfvmyyi7wtpxqdggweilgak0d0ys35ktrf9ssm6jyafxrmn3ipwkvghhm2ilbx4sckpnlditunervtidvw2xlww1hcpbrmi40hhs.png" alt="" />
+                <img className="nav__avatar"
+                    src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="" />
+
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default Nav
